ddde8c8078cc39c97dec55acee99b2baf87608a2
fatihcaliss <fthcls@gmail.com>
2023-12-18 16:01:45 +0300

    create new users

--- util/auth.js
+++ util/auth.js
@@ -2,14 +2,15 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyCqpLFYUMkz-I0Nus9el_EY3GHvVCW_1B4";
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
+  //   console.log(email, password);
   const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=" +
-      API_KEY,
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
       email: email,
       password: password,
       returnSecureToken: true,
     }
   );
+  //   console.log(response);
 }
